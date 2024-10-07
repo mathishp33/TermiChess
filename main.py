f51e88b9bf3e754c52805e3b28ad91e2eb0be15d
Mathis <69140979+mathishp33@users.noreply.github.com>
2024-10-07 20:39:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class Aplication():
     def __init__(self):
-        self.RES = self.WIDTH, self.HEIGHT = 1600, 900
+        self.RES = self.WIDTH, self.HEIGHT = 800, 800
         self.screen = pg.display.set_mode(self.RES)
         self.clock = pg.time.Clock()
         pg.init()
@@ -31,10 +31,15 @@ class Aplication():
         self.clock.tick(self.FPS)
     
     def drawing(self, a):
-        for i in range(self.WIDTH//(a*2)):
-            for j in range(self.HEIGHT//(a)):
-                pg.draw.rect(self.screen, (0, 0, 0), pg.Rect(i*a, j*a, a, a))
-                pg.draw.rect(self.screen, (255, 255, 255), pg.Rect(i*a +a, j*a, a, a))
+        for i in range(0, self.WIDTH//(a), 2):
+            for j in range(0, self.HEIGHT//(a), 1):
+                if j//2 == j/2:
+                    pg.draw.rect(self.screen, (0, 0, 0), pg.Rect(i*a, j*a, a, a))
+                    pg.draw.rect(self.screen, (255, 255, 255), pg.Rect(i*a +a, j*a, a, a))
+                else:
+                    pg.draw.rect(self.screen, (255, 255, 255), pg.Rect(i*a, j*a, a, a))
+                    pg.draw.rect(self.screen, (0, 0, 0), pg.Rect(i*a, j*a +a, a, a))
+
 
 
 if __name__ == '__main__':
