b4e1af47c3aa2bf5d9e031a39d8eb0aaa3bf18a9
Mathis <69140979+mathishp33@users.noreply.github.com>
2024-10-07 19:33:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Aplication():
         self.RES = self.WIDTH, self.HEIGHT = 1600, 900
         self.screen = pg.display.set_mode(self.RES)
         self.clock = pg.time.Clock()
+        pg.init()
         self.FPS = 120
         self.running = True
         self.clicking = False
@@ -15,7 +16,7 @@ class Aplication():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.running = False
-                pg.quit()
+                exit()
             if pg.mouse.get_pressed()[0]:
                 self.clicking = True
             else: 
@@ -24,6 +25,7 @@ class Aplication():
         # do stuff
         #
         #
+        pg.display.flip()
         pg.display.set_caption(str(round(self.clock.get_fps(), 3)))
         self.clock.tick(self.FPS)
 
