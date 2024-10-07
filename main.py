bf50182ca5f63358f9cceb5148c4be1e80327770
Mathis <69140979+mathishp33@users.noreply.github.com>
2024-10-07 19:25:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,14 @@ class Aplication():
         self.screen = pg.display.set_mode(self.RES)
         self.clock = pg.time.Clock()
         self.FPS = 120
-        self.
+        self.running = True
+        self.clicking = False
+
+    def update(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.running = False
+            if pg.mouse.get_pressed()[0]:
+                self.clicking = True
+            else: 
+                self.clicking = False
