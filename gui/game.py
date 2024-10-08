b30a79a1f7db835ab925fb37197fef0903999bfa
Mathis <69140979+mathishp33@users.noreply.github.com>
2024-10-08 12:51:28 +0200

    Update game.py

--- gui/game.py
+++ gui/game.py
@@ -1,9 +1,8 @@
 import pygame as pg
 import os
-import numpy as np
 
-WHITE = 16
-BLACK = 0
+WHITE = 0
+BLACK = 1
 
 KING = 1
 QUEEN = 2
@@ -14,16 +13,20 @@ PAWN = 6
 
 class Game():
     def __init__(self):
-        self.board = np.ndarray(shape=(8, 8), dtype= int)
-        self.board[0][0] = WHITE | PAWN
+        self.board = [[0 for i in range(8)] for i in range(8)]
+        self.board[0][0] = [WHITE, PAWN]
         
 
 class Pieces():
     def __init__(self):
         self.board = Game()
-        self.pieces_img, index = {}, 0b0000
-        for i in ['white_1', 'black_1', 'white_2', 'black_2', 'white_3', 'black_3', 'white_4', 'black_4', 'white_5', 'black_5', 'white_6', 'black_6' ]:
-            index = bin(int(index)-16) if 'white' in i else bin(int(index)-16) 
-            self.pieces_img[index] = pg.image.load('\\ressources\\' + i + '.png')
-            self.pieces = [self.pieces.append([i, j, self.board.board[i][j]]) for i in len(8) for j in range(8) if not self.board.board[i][j] == 0]
+        self.pieces_index = ['white_1', 'black_1', 'white_2', 'black_2', 'white_3', 'black_3', 'white_4', 'black_4', 'white_5', 'black_5', 'white_6', 'black_6' ]
+        self.pieces = []
+        for i in range(8):
+            for j in range(8):
+                if not self.board.board[i][j] == 0:
+                    name_of = self.pieces_index[self.board.board[i][j][0]+self.board.board[i][j][1]*2-1]
+                    item = [i, j,pg.image.load(('ressources\\' + name_of + '.png'))]
+                    self.pieces.append(item)
+        
             
