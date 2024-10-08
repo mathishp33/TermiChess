caef09914a33c5ff2be06de87985b30e5e3d593a
Mathis <69140979+mathishp33@users.noreply.github.com>
2024-10-08 12:56:30 +0200

    Update game.py

--- gui/game.py
+++ gui/game.py
@@ -13,7 +13,13 @@ PAWN = 6
 
 class Game():
     def __init__(self):
-        self.board = [[0 for i in range(8)] for i in range(8)]
+        self.board = [[[5,1], [4,1], [3,1], [2,1], [1,1], 3,1, 4,1, 5+1],
+                      [6,1, 6,1, 6,1, 6,1, 6,1, 6,1, 6,1, 6,1],
+                      [0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0],
+                      [6, 6, 6, 6, 6, 6, 6, 6],
+                      [5, 4, 3, 1, 2, 3, 4, 5]]
         self.board[0][0] = [WHITE, PAWN]
         
 
@@ -28,5 +34,6 @@ class Pieces():
                     name_of = self.pieces_index[self.board.board[i][j][0]+self.board.board[i][j][1]*2-1]
                     item = [i, j,pg.image.load(('ressources\\' + name_of + '.png'))]
                     self.pieces.append(item)
+        print(self.pieces)
         
             
