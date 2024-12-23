97aa824544a9e7d44f7eb60db41ba44c4382a149
dgdzd <noah.campagne@icloud.com>
2024-12-23 16:01:39 +0100

    Started working on new move system + fixed bugs

--- chess/game.py
+++ chess/game.py
@@ -4,7 +4,6 @@ import platform
 from chess.utils import *
 from gui.events.keyboard_events import *
 from gui.events.event import *
-import struct
 
 
 '''
@@ -156,9 +155,7 @@ class Game():
             if t == KNIGHT: return "n"
             if t == ROOK: return "r"
             if t == PAWN: return "p"
-        return " "
-
-        
+        return " "       
     
 @event_listener
 def onKeyPress(event: KeyPressEvent):
@@ -171,6 +168,106 @@ def onKeyPress(event: KeyPressEvent):
         if i > 0:
             Game.current.move -= 1
             Game.current.moves[i-1].undo()
+
+
+"""
+Moves will be stored either as 16-bit or 32-bit integers in the future, storing the following informations :
+- The starting square, 'from', will be stored in the first 6 bits (starting from the right).
+- The ending square, 'to', will be stored in the next 6 bits.
+- The move's flags, stored in a 4-bit bitfield, will be organized as follows :
+    - The first bit starting from the left will be set if the move is a promotion.
+    - The next bit will be set if the move is a capture.
+    - The 2 next bits are special bits, used for different purposes depending on the first 2 bits.
+- In case of a 32-bit integer, the following informations will be stored after :
+    - The main piece's state, encoded on 5 bits.
+    - The other concerned piece's state, encoded on 5 bits (0b00000 if no piece).
+    - The other concerned piece's index, encoded on 6 bits (0b000000 if no piece).
+"""
+
+QUIET_MOVE = 0
+DOUBLE_PAWN_PUSH = 1
+KING_CASTLE = 2
+QUEEN_CASTLE = 3
+CAPTURE = 4
+EN_PASSANT = 5
+KNIGHT_PROMOTION = 8
+BISHOP_PROMOTION = 9
+ROOK_PROMOTION = 10
+QUEEN_PROMOTION = 11
+KNIGHT_PROMOTION_CAPTURE = 12
+BISHOP_PROMOTION_CAPTURE = 13
+ROOK_PROMOTION_CAPTURE = 14
+QUEEN_PROMOTION_CAPTURE = 15
+
+def move16(from_: int, to: int, flags: int) -> np.int16:
+    """
+    Returns a 16-bit integer representing a move.
+    """
+    return np.int16((flags << 12) | (from_ << 6) | to)
+
+def move16_from_move32(move: np.int32) -> np.int16:
+    return move16(move32_get_from(move), move32_get_to(move), move32_get_flags(move))
+
+def move16_get_flags(move: np.int16) -> np.int16:
+    return (move >> 12) & 0xf
+
+def move16_get_from(move: np.int16) -> np.int16:
+    return (move >> 6) & 0x3f
+
+def move16_get_to(move: np.int16) -> np.int16:
+    return move & 0x3f
+
+def move16_is_promotion(move: np.int16) -> bool:
+    return move16_get_flags(move) >= 8
+
+def move16_is_capture(move: np.int16) -> bool:
+    return move16_get_flags(move) == CAPTURE or move16_get_flags(move) >= 12
+
+def move16_is_castle(move: np.int16) -> bool:
+    return move16_get_flags(move) == KING_CASTLE or move16_get_flags(move) == QUEEN_CASTLE
+
+
+def move32(from_: int, to: int, flags: int, piece_state: int = -1, other_piece_state: int = 0, other_piece_index: int = 0) -> np.int32:
+    """
+    Returns a 32-bit integer representing a move.
+    """
+    if piece_state == -1:
+        piece_state = Game.current.board[from_]
+    return np.int32((other_piece_index << 26) | (other_piece_state << 21) | (piece_state << 16) | (flags << 12) | (from_ << 6) | to)
+
+def move32_from_move16(move: np.int16, piece_state: int = -1, other_piece_state: int = 0, other_piece_index: int = 0) -> np.int32:
+    return move32(move16_get_from(move), move16_get_to(move), move16_get_flags(move), piece_state, other_piece_state, other_piece_index)
+
+def move32_get_piece_state(move: np.int32) -> np.int32:
+    return (move >> 26) & 0x3f
+
+def move32_get_other_piece_state(move: np.int32) -> np.int32:
+    return (move >> 21) & 0x1f
+
+def move32_get_piece_state(move: np.int32) -> np.int32:
+    return (move >> 16) & 0x1f
+
+def move32_get_flags(move: np.int32) -> np.int32:
+    return (move >> 12) & 0xf
+
+def move32_get_from(move: np.int32) -> np.int32:
+    return (move >> 6) & 0x3f
+
+def move32_get_to(move: np.int32) -> np.int32:
+    return move & 0x3f
+
+def move32_is_promotion(move: np.int32) -> bool:
+    return move16_get_flags(move) >= 8
+
+def move32_is_capture(move: np.int32) -> bool:
+    return move16_get_flags(move) == CAPTURE or move16_get_flags(move) >= 12
+
+def move32_is_castle(move: np.int32) -> bool:
+    return move16_get_flags(move) == KING_CASTLE or move16_get_flags(move) == QUEEN_CASTLE
+
+"""
+This class will be deprecated soon.
+"""
  
 class Move:
     def __init__(self, ibefore: int, iafter: int, eaten_piece: int = 0, eaten_i = -1, castle: int = -1, en_passant: bool = False):
@@ -237,10 +334,11 @@ class MoveGenerator:
 
     def __init__(self, parent: Game):
         self.parent = parent
-        self.checked = False
+        self.check_count = 0
         self.checker = None
         self.pinned_pieces = []
         self.pin_lines = [[] for i in range(8)]
+        self.anti_retreat_squares = []
         self.attacked_squares = [[], []]
         self.precompute_move_data()
         self.locate_pieces()
@@ -249,13 +347,14 @@ class MoveGenerator:
     def update_moves(self, team):
         self.pin_lines = [[] for i in range(8)]
         self.pinned_pieces = []
-        self.checked = False
+        self.anti_retreat_squares = []
+        self.check_count = 0
         self.generate_attacked_squares(team)
         self.generate_pins(team)
         self.generate_legal_moves(team)
 
     def generate_pins(self, team):
-        if not self.checked:
+        if self.check_count == 0:
             k_index = -1
             for i in MoveGenerator.tracked_pieces:
                 if self.parent.board[i] == KING | team:
@@ -284,15 +383,15 @@ class MoveGenerator:
                         if i < 4:
                             if (type_ == ROOK or type_ == QUEEN) and pinned_piece != 0:
                                 if pinned_piece == 0:
-                                    self.checked = True
+                                    self.check_count += 1
                                     self.checker = square
                                     line.append(square + dir)
                                 add = True
                             break
                         else:
-                            if (type_ == BISHOP or type_ == QUEEN):
+                            if (type_ == BISHOP or type_ == QUEEN) and pinned_piece != 0:
                                 if pinned_piece == 0:
-                                    self.checked = True
+                                    self.check_count += 1
                                     self.checker = square
                                     line.append(square + dir)
                                 add = True
@@ -306,7 +405,7 @@ class MoveGenerator:
                     self.pinned_pieces.append(i)
 
 
-    def generate_legal_moves(self, team):
+    def generate_legal_moves(self, team) -> list[Move]:
         self.moves: list[Move] = []
         for i in MoveGenerator.tracked_pieces:
             piece = self.parent.board[i]
@@ -324,14 +423,14 @@ class MoveGenerator:
                         square = i + k * DIRS_OFFSET[j]
                         target_piece = self.parent.board[square]
                         if Game.get_piece_team(target_piece) == team_: break
-                        if not i in self.pinned_pieces and not self.checked:
+                        if not i in self.pinned_pieces and self.check_count == 0:
                             self.moves.append(Move(i, square))
-                        elif self.checked and self.is_square_in_pins(square):
+                        elif self.check_count > 0 and self.is_square_in_pins(square):
                             self.moves.append(Move(i, square))
                         if Game.get_piece_team(target_piece) != 0: break
             
             elif type_ == KNIGHT:
-                if not i in self.pinned_pieces and not self.checked:
+                if not i in self.pinned_pieces and self.check_count == 0:
                     start_index = 0 if i % 8 > 1 else 2 if i % 8 > 0 else 4
                     end_index = 8 if i % 8 < 6 else 6 if i % 8 < 7 else 4
                     for j in range(start_index, end_index):
@@ -340,7 +439,7 @@ class MoveGenerator:
                         target_piece = self.parent.board[square]
                         if Game.get_piece_team(target_piece) == team_: continue
                         self.moves.append(Move(i, square))
-                elif self.checked:
+                elif self.check_count > 0:
                     start_index = 0 if i % 8 > 1 else 2 if i % 8 > 0 else 4
                     end_index = 8 if i % 8 < 6 else 6 if i % 8 < 7 else 4
                     for j in range(start_index, end_index):
@@ -361,7 +460,8 @@ class MoveGenerator:
                         target_piece = self.parent.board[square]
                         if Game.get_piece_team(target_piece) == team_: continue
                         if self.attacked_squares[0 if team_ == WHITE else 1][square] == 0:
-                            self.moves.append(Move(i, square))
+                            if self.check_count == 0 or not square in self.anti_retreat_squares:
+                                self.moves.append(Move(i, square))
 
             elif type_ == PAWN:
                 offsets = (-9, -7, -8, -16, 6) if team_ == WHITE else (7, 9, 8, 16, 1)
@@ -371,9 +471,9 @@ class MoveGenerator:
                     target_team = Game.get_piece_team(target_piece)
                     if target_team != team_ and target_team != 0:
                         if i % 8 > 0:
-                            if not i in self.pinned_pieces and not self.checked:
+                            if not i in self.pinned_pieces and self.check_count == 0:
                                 self.moves.append(Move(i, square))
-                            elif self.checked and self.is_square_in_pins(square):
+                            elif self.check_count > 0 and self.is_square_in_pins(square):
                                 self.moves.append(Move(i, square))
                 square = i + offsets[1]
                 if square > -1 and square < 64:
@@ -381,17 +481,17 @@ class MoveGenerator:
                     target_team = Game.get_piece_team(target_piece)
                     if target_team != team_ and target_team != 0:
                         if i % 8 < 7:
-                            if not i in self.pinned_pieces and not self.checked:
+                            if not i in self.pinned_pieces and self.check_count == 0:
                                 self.moves.append(Move(i, square))
-                            elif self.checked and self.is_square_in_pins(square):
+                            elif self.check_count and self.is_square_in_pins(square):
                                 self.moves.append(Move(i, square))
                 square = i + offsets[2]
                 if square > -1 and square < 64:
                     blocking_piece = self.parent.board[square]
                     if blocking_piece == 0:
-                        if not i in self.pinned_pieces and not self.checked:
+                        if not i in self.pinned_pieces and self.check_count == 0:
                             self.moves.append(Move(i, square))
-                        elif self.checked and self.is_square_in_pins(square):
+                        elif self.check_count > 0 and self.is_square_in_pins(square):
                             self.moves.append(Move(i, square))
                     else:
                         continue
@@ -399,13 +499,13 @@ class MoveGenerator:
                     square = i + offsets[3]
                     blocking_piece = self.parent.board[square]
                     if blocking_piece == 0:
-                        if not i in self.pinned_pieces and not self.checked:
+                        if not i in self.pinned_pieces and self.check_count == 0:
                             self.moves.append(Move(i, square))
-                        elif self.checked and self.is_square_in_pins(square):
+                        elif self.check_count > 0 and self.is_square_in_pins(square):
                             self.moves.append(Move(i, square))
         return self.moves
     
-    def is_square_in_pins(self, square: int, lines: str = "all"):
+    def is_square_in_pins(self, square: int, lines: str = "all") -> bool:
         for i in range(len(self.pin_lines)):
             line = self.pin_lines[i]
             if (lines == "all" or lines == "straight") and i < 4:
@@ -437,7 +537,14 @@ class MoveGenerator:
                 for j in range(1, int(distance)+1):
                     square = index + j * DIRS_OFFSET[i]
                     self.attacked_squares[0 if t_index == 1 else 1][square] += 1
-                    if self.parent.board[square] != 0: break
+                    piece = self.parent.board[square]
+                    if piece != 0:
+                        if Game.get_piece_team(piece) != team:
+                            if Game.get_piece_type(piece) == KING:
+                                self.check_count += 1
+                                self.checker = index
+                                self.anti_retreat_squares.append(square + DIRS_OFFSET[i])
+                        break
             
         elif type_ == KNIGHT:
             start_index = 0 if index % 8 > 1 else 2 if index % 8 > 0 else 4
@@ -449,7 +556,7 @@ class MoveGenerator:
                 target = self.parent.board[square]
                 if Game.get_piece_team(target) != team:
                     if Game.get_piece_type(target) == KING:
-                        self.checked = True
+                        self.check_count += 1
                         self.checker = index
                         self.pin_lines[0].append(index)
                         self.pin_lines[4].append(index)
@@ -471,7 +578,7 @@ class MoveGenerator:
                     target = self.parent.board[square]
                     if Game.get_piece_team(target) != team:
                         if Game.get_piece_type(target) == KING:
-                            self.checked = True
+                            self.check_count += 1
                             self.checker = index
             square = index + offsets[1]
             if square > -1 and square < 64:
@@ -480,7 +587,7 @@ class MoveGenerator:
                     target = self.parent.board[square]
                     if Game.get_piece_team(target) != team:
                         if Game.get_piece_type(target) == KING:
-                            self.checked = True
+                            self.check_count += 1
                             self.checker = index
                             
 
